e4e14feabd5171d0ecc0a66615c5d9bc9c5631a4
Lh Long <lhlong09t4@gmail.com>
2021-03-21 09:18:49 +0700

    v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="vn_fullname_generator", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Lh Long",
     author_email="lhlong@github.com",
     description="Generate Vietnamese Fullname",
